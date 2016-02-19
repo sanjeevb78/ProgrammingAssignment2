d1e4405bd04089e32e94301d6cdbb0735ef35670
Sanjeev Bhattacharjee <jeev_7@yahoo.com>
2016-02-19 14:46:29 +0530

    Coursera Week3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
-## functions do
+# The below function will take a matrix as input and will reverse the matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()){
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve(m)
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+#Here we will use previous function to inverse the matrix
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x = matrix()){
         ## Return a matrix that is the inverse of 'x'
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve(m)
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
